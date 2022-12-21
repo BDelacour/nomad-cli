5f0933c9b33abbbaa9435549db3fcb928a741afc
Benjamin Delacour <bdelacour33@gmail.com>
2022-12-21 20:46:00 +0100

    ci: fix workflow and remove debug from python

--- generate-build-matrix.py
+++ generate-build-matrix.py
@@ -35,10 +35,10 @@ def generate_build_matrix(already_published, all_tags):
     latest = all_tags[0]
     to_publish = []
     for tag in not_published:
-        image_tag = f"testing/{DOCKER_IMAGE}:{tag}"
+        image_tag = f"{DOCKER_NAMESPACE}/{DOCKER_IMAGE}:{tag}"
         if tag == latest:
             # Github doesn't allow newline so we encode it
-            image_tag += f"%0Atesting/{DOCKER_IMAGE}:latest"
+            image_tag += f"%0A{DOCKER_NAMESPACE}/{DOCKER_IMAGE}:latest"
         to_publish.append(image_tag)
     matrix = {
       "tags": to_publish
